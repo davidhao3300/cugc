8a914df0d408e77a2005c7607da0866408a77ff6
David H <davidhao3300@yahoo.com>
2015-10-08 22:01:26 -0400

    Initial front-end

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,5 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres
 db = SQLAlchemy(app)
 
 @app.route('/')
-def home():
-    return 'Hello world'
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
