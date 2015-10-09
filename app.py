2308df239e17c9e426b2819b2ef5876902116285
David H <davidhao3300@yahoo.com>
2015-10-08 22:40:17 -0400

    Add basic information

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import arrow
+import datetime
 import os
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -10,7 +12,11 @@ import models
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Get next Friday
+    today = datetime.date.today()
+    friday = today + datetime.timedelta( (4-today.weekday()) % 7 )
+    friday = arrow.get(friday).format('MMMM D, YYYY')
+    return render_template('index.html', meeting_date=friday)
 
 if __name__ == '__main__':
     app.run(debug=True)
