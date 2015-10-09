2a392375199acb24d30600da165c5de6a373bb13
David H <davidhao3300@yahoo.com>
2015-10-08 21:35:19 -0400

    Add games model

---
+++ models/game.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    url = db.Column(db.String, unique=True)
+    pic_url = db.Column(db.String)
+
+    def __init__(self, name, url, pic_url):
+        self.name = name
+        self.url = url
+        self.pic_url = pic_url
+
+    def __repr__(self):
+        return '<Game %r>' % self.name
